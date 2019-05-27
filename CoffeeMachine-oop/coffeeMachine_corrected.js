32e0862bf4e53638d98d35b68ab9033e7e0b31d5
Svetlana Tovt <ms.tsp00@gmail.com>
2019-05-27 07:16:21 +0300

    coffeeMachine corrected

--- CoffeeMachine-oop/coffeeMachine_corrected.js
+++ CoffeeMachine-oop/coffeeMachine_corrected.js
@@ -1,70 +1,53 @@
 let latte = {
     name: 'latte',
-    water: 50,
+    water: 500,
     coffeeBeans: 7,
     milk: 150
 }
-  
-let americano = {
-    name: 'americano',
-    water: 100,
-    coffeeBeans: 14,
-    milk: 0
-}
-
-let espresso = {
-    name: 'espresso',
-    water: 30,
-    coffeeBeans: 7,
-    milk: 0
-}
 
 class CheckWater {
-
-    constructor(waterInStorege) {
-        this.water = waterInStorege;
-    }
+  constructor(waterInStorege) {
+    this.water = waterInStorege;
+  }
   
-    boil (water) {
-        if(this.water >= water) {
-            return true;
-        } else {
-            console.log(`There isn't enough water. Please refill it`);
-            return false;
-        }     
+  boil (water) {
+    if(this.water >= water) {
+      return true;
+    } else {
+      console.log(`There isn't enough water. Please refill it`);
+      return false;
     }
+  }
 }
 
 class CheckCoffeeBeans {
-
-    constructor(coffeeBeansInStorege) {
-        this.coffeeBeans = coffeeBeansInStorege;
+  constructor(coffeeBeansInStorege) {
+    this.coffeeBeans = coffeeBeansInStorege;
+  }
+  
+  grind (coffeeBeans) {
+    if(this.coffeeBeans >= coffeeBeans) {
+      return true;
+    } else {
+      console.log(`There isn't enough coffeeBeans. Please refill it`);
+      return false;
     }
-
-    grind (coffeeBeans) {
-        if(this.coffeeBeans >= coffeeBeans) {
-            return true;
-        } else {
-            console.log(`There isn't enough coffeeBeans. Please refill it`);
-            return false;
-        }
-    }
+  }
 }
 
 class CheckMilk {
-
-    constructor(milkInStorege) {
-        this.milk = milkInStorege;
+  
+  constructor(milkInStorege) {
+    this.milk = milkInStorege;
+  }
+   add (milk) {
+    if(this.milk >= milk) {
+      return true;
+    } else {
+      console.log(`There isn't enough coffeeBeans. Please refill it`);
+      return false;
     }
-
-    add (milk) {
-        if(this.milk >= milk) {
-            return true;
-        } else {
-            console.log(`There isn't enough coffeeBeans. Please refill it`);
-            return false;
-        }
-    }
+   }
 }
 
 
@@ -76,28 +59,31 @@ class CoffeeMachine {
         this.milk = 300;
     }
 
-    applyFor (coffeeType) {
-        let boilWater = new CheckWater(this.water).boil(coffeeType.water);
-        let grindCoffeeBeans = new CheckCoffeeBeans(this.coffeeBeans).grind(coffeeType.coffeeBeans);
-        let addMilk = new CheckMilk(this.milk).add(coffeeType.milk);
-        let makeCoffee = boilWater && grindCoffeeBeans && addMilk ? 'is done' : `can't be done`;
+  makeCoffee (coffeeType) {
+    
+    let boilWater = new CheckWater(this.water).boil(coffeeType.water);
+    let grindCoffeeBeans = new CheckCoffeeBeans(this.coffeeBeans).grind(coffeeType.coffeeBeans);
+    let addMilk = new CheckMilk(this.milk).add(coffeeType.milk);
 
-        this.water -= coffeeType.water;
-        this.coffeeBeans -= coffeeType.coffeeBeans;
-        this.milk -= coffeeType.milk;
+    let makeCoffee = boilWater && grindCoffeeBeans && addMilk ? 'is done' : `can't be done`;
+    
+    this.water -= coffeeType.water;
+    this.coffeeBeans -= coffeeType.coffeeBeans;
+    this.milk -= coffeeType.milk;
 
-        return `Your ${coffeeType.name} ${makeCoffee} `;
-    }
+    
+    return `Your ${coffeeType.name} ${makeCoffee} `;
+  }
   
-    refillComponents() {
-        this.water = 1000;
-        this.coffeeBeans = 500;
-        this.milk = 300;
-        console.log('All the components are refilled');
-        }
-    }
+  refillComponents() {
+    this.water = 1000;
+    this.coffeeBeans = 500;
+    this.milk = 300;
+    console.log('All the components are refilled');
+  }
+}
 
 const myCoffeeMaker = new CoffeeMachine();
-console.log(myCoffeeMaker.applyFor(latte));
-console.log(myCoffeeMaker.applyFor(latte));
-console.log(myCoffeeMaker.applyFor(latte));
+console.log(myCoffeeMaker.makeCoffee(latte));
+console.log(myCoffeeMaker.makeCoffee(latte));
+console.log(myCoffeeMaker.makeCoffee(latte));
